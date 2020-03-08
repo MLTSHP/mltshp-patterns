f0724db060ed5ec890ee814e2da31ee387b721ee
Scott Vandehey <fake-scott@spaceninja.com>
2020-03-08 15:17:59 -0700

    add icons

--- .storybook/main.js
+++ .storybook/main.js
@@ -12,12 +12,25 @@ module.exports = {
         sassLoaderOptions: {
           // Dart Sass performs much better than Node Sass
           implementation: require('sass'),
-          // sassOptions: {
-          //   // Import Theo design tokens as SCSS variables
-          //   importer: [require("./theo-importer")]
-          // }
         },
       },
     },
   ],
+  webpackFinal: async config => {
+    // Remove default SVG processing from default config.
+    // @see https://github.com/storybookjs/storybook/issues/5708#issuecomment-515384927
+    config.module.rules = config.module.rules.map(data => {
+      if (/svg\|/.test(String(data.test))) {
+        data.test = /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/;
+      }
+      return data;
+    });
+    // Push new rules
+    config.module.rules.push({
+      // Optimize and process SVGs as React elements for use in documentation
+      test: /\.svg$/,
+      use: '@svgr/webpack',
+    });
+    return config;
+  },
 };
