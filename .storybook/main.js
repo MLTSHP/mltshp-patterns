d620ca6a61cc4a5f90282363e7cf124dd22b253f
Scott Vandehey <fake-scott@spaceninja.com>
2020-03-07 19:06:42 -0800

    add addons and examples

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,11 @@
 module.exports = {
-  stories: ["../src/**/*.stories.[tj]s"],
+  stories: ["../src/**/*.stories.(js|mdx)"],
   addons: [
-    // "@storybook/addon-a11y/register",
-    // "@storybook/addon-backgrounds/register",
-    // "@storybook/addon-docs",
-    // "@storybook/addon-knobs/register",
-    // "@storybook/addon-viewport/register",
+    "@storybook/addon-a11y/register",
+    "@storybook/addon-backgrounds/register",
+    "@storybook/addon-docs",
+    "@storybook/addon-knobs/register",
+    "@storybook/addon-viewport/register",
     {
       name: "@storybook/preset-scss",
       options: {
