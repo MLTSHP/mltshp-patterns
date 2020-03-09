aade3e9fac2952ba25bdb11ae1cc8ee58f30b73f
Scott Vandehey <fake-scott@spaceninja.com>
2020-03-09 16:53:01 -0700

    bringing over the base styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,16 @@
 import { addDecorator, addParameters } from '@storybook/html';
 import { withA11y } from '@storybook/addon-a11y';
-// import * as colors from "../src/design-tokens/colors.yml";
+import * as colors from '../src/design-tokens/colors.yml';
 // import "focus-visible";
+import './preview.scss';
 
 // Add decorators to all stories
 addDecorator(withA11y);
 
 // Set choosable backgrounds for stories
 const backgrounds = [
-  { name: 'light', value: '#eeeeee', default: true },
-  { name: 'dark', value: '#222222' },
+  { name: 'light', value: colors.pageBgLight, default: true },
+  { name: 'dark', value: colors.pageBgDark },
 ];
 
 addParameters({ backgrounds });
