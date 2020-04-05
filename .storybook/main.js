959d0fa6f681ad6373154c999c29429a80e7b9cc
Scott Vandehey <fake-scott@spaceninja.com>
2020-04-04 17:47:29 -0700

    Add PostCSS and SVG Inlining
    Adds PostCSS to our Storybook CSS loader stack, and adds the PostCSS
    Inline SVG plugin, which allows using SVG images in CSS files.

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,27 +8,11 @@ module.exports = {
     '@storybook/addon-docs',
     '@storybook/addon-knobs/register',
     '@storybook/addon-viewport/register',
-    {
-      name: '@storybook/preset-scss',
-      options: {
-        sassLoaderOptions: {
-          // Dart Sass performs much better than Node Sass
-          implementation: require('sass'),
-          sassOptions: {
-            importer: [
-              require('../glob-sass-importer'),
-              // Import Theo design tokens as SCSS variables
-              require('../.theo/sass-importer'),
-            ],
-          },
-        },
-      },
-    },
   ],
-  webpackFinal: async config => {
+  webpackFinal: async (config) => {
     // Remove default SVG processing from default config.
     // @see https://github.com/storybookjs/storybook/issues/5708#issuecomment-515384927
-    config.module.rules = config.module.rules.map(data => {
+    config.module.rules = config.module.rules.map((data) => {
       if (/svg\|/.test(String(data.test))) {
         data.test = /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/;
       }
@@ -36,6 +20,36 @@ module.exports = {
     });
     // Push new rules
     config.module.rules.push(
+      {
+        test: /\.s[ca]ss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              // Lets CSS loader know there are two loaders left that may be
+              // handling imports.
+              // @see https://github.com/webpack-contrib/css-loader#importloaders
+              importLoaders: 2,
+            },
+          },
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              // Dart Sass performs much better than Node Sass
+              implementation: require('sass'),
+              sassOptions: {
+                importer: [
+                  require('../glob-sass-importer'),
+                  // Import Theo design tokens as SCSS variables
+                  require('../.theo/sass-importer'),
+                ],
+              },
+            },
+          },
+        ],
+      },
       {
         // Optimize and process SVGs as React elements for use in documentation
         test: /\.svg$/,
