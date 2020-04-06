93fe6f63c081207c8e93ab50ae2a2934a6b562d9
Scott Vandehey <fake-scott@spaceninja.com>
2020-04-06 16:48:24 -0700

    add additional form documentation

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   trailingComma: 'es5',
   singleQuote: true,
+  overrides: [
+    {
+      files: '*.html',
+      options: {
+        // need this to prevent prettier from adding semicolons to html files
+        parser: 'html',
+      },
+    },
+  ],
 };
