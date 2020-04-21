150d72975b6d7c8e89be5318187cc54dc9ae278a
Scott Vandehey <fake-scott@spaceninja.com>
2020-04-21 14:58:04 -0700

    use design token breakpoints in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,8 @@
 import { addDecorator, addParameters } from '@storybook/html';
 import { withA11y } from '@storybook/addon-a11y';
 import * as colors from '../src/design-tokens/colors.yml';
+import * as breakpoints from '../src/design-tokens/breakpoint.yml';
+import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
 // import "focus-visible";
 import './preview.scss';
 
@@ -13,4 +15,24 @@ const backgrounds = [
   { name: 'dark', value: colors.colorPageBgDark },
 ];
 
-addParameters({ backgrounds });
+// Create viewports using widths defined in design tokens
+const breakpointViewports = Object.keys(breakpoints).map((name) => {
+  return {
+    name: `Breakpoint ${name.toUpperCase().substring(10)}`,
+    styles: {
+      width: breakpoints[name],
+      height: '100%',
+    },
+    type: 'other',
+  };
+});
+
+addParameters({
+  backgrounds,
+  viewport: {
+    viewports: {
+      ...breakpointViewports,
+      ...MINIMAL_VIEWPORTS,
+    },
+  },
+});
