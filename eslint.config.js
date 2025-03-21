ef7a91c96fece5c095d3e358cad489831ec8cba7
Scott Vandehey <fake-scott@spaceninja.com>
2025-03-20 19:36:14 -0700

    update config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import globals from 'globals';
 import js from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default [
 	js.configs.recommended,
